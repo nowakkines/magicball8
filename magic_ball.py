2c8bb9207ceb3eeb44daf49815a6c791bfb780e1
ap7kj <spearekurwa@yandex.com>
2022-08-03 15:35:19 +0300

    feat: add playing function

--- magic_ball.py
+++ magic_ball.py
@@ -11,35 +11,16 @@ answers = ["Undoubtedly", "I think so", "It's not clear yet, try again", "Don't 
 
 
 def hello():
-    global name
     print('''
 *** Hello World, I am a magic ball, and
 I know the answer to any of your questions.
 Just write your question ***
     ''')
-    match 'Max':
-        case len(1):
-            print('123')
-    # name = '' #input('[?] Tell me your name >> ')'
-    # print(f'Hello, {name}!\n')
-
+    name = input('[?] Tell me your name >> ')
+    print(f'Hello, {name}!')
 
 def playing():
     global name
     ask = 'Am I stupid?' #input('[?] What do you want to ask me? >> ')
     print(f'{choice(answers)}, {name}!')
-    ask_continue()
 
-def ask_continue():
-    match input('[?] Again? (+/-) '):
-        case '+':
-            playing()
-        case '-':
-            print('Come back if you have any questions!')
-        case _:
-            print('Something\'s wrong. Don\'t enter it.')
-            ask_continue()
-
-
-hello()
-playing()
